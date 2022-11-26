00be021f6b44791faea7da44ca482484df4d4f13
asafr <asaf.rehavi@gmail.com>
2022-11-26 14:20:44 +0200

    first commit

---
+++ RabbitMq/Sender.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Sender
+{
+    public Sender()
+    {
+        SendMessageToNodeJs();
+
+
+    }
+
+    private async void SendMessageToNodeJs()
+    {
+        var timer = new PeriodicTimer(TimeSpan.FromSeconds(10));
+
+        while (await timer.WaitForNextTickAsync())
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "c#ToNodeJs",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello from c#" + DateTime.Now.ToString();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "c#ToNodeJs",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+           
+
+        }
+
+    }
+
+}
