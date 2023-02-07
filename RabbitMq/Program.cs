f9203747b141d1193bc1c8743403e50688d8a72d
asaf.rehavi <asaf_r@ftlsoft.com>
2023-02-07 11:57:53 +0200

    Remove commets

--- RabbitMq/Program.cs
+++ RabbitMq/Program.cs
@@ -1,41 +1,4 @@
-﻿//// See https://aka.ms/new-console-template for more information
-
-
-
-//using RabbitMq;
-//using RabbitMQ.Client.Events;
-//using RabbitMQ.Client;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-
-//new Sender();
-//var factory = new ConnectionFactory() { HostName = "localhost" };
-//using (var connection = factory.CreateConnection())
-//using (var channel = connection.CreateModel())
-//{
-//    channel.QueueDeclare(queue: "FromNodeToC#",
-//                         durable: false,
-//                         exclusive: false,
-//                         autoDelete: false,
-//                         arguments: null);
-
-//    var consumer = new EventingBasicConsumer(channel);
-//    consumer.Received += (model, ea) =>
-//    {
-//        var body = ea.Body.ToArray();
-//        var message = Encoding.UTF8.GetString(body);
-//        Console.WriteLine(" [x] Received {0}", message);
-//    };
-//    channel.BasicConsume(queue: "FromNodeToC#",
-//                         autoAck: true,
-//                         consumer: consumer);
-//}
-
-//Console.WriteLine(" Press [enter] to exit.");
-//Console.ReadLine();
+﻿
 
 
 
